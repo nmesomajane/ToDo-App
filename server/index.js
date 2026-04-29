7f77944fa41b048820466c3442b21f7c9ad2fb7f
enactus-futo <nnopujane@gmail.com>
2026-04-29 19:05:26 +0100

    Added render server url

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors({
-  origin: [ 'http://localhost:5173'],
+  origin: [ 'http://localhost:5173', 'https://todo-app-4d5k.onrender.com' ,],
   credentials: true
 }));
 
